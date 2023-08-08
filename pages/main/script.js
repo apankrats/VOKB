38446a0ca2a52801b361c4c84beea67ca49ae15e
apankrats <aliena0798@mail.ru>
2023-08-08 17:16:37 +0300

    open and close menu

--- pages/main/script.js
+++ pages/main/script.js
@@ -1,18 +1,28 @@
 let navRefs = document.querySelectorAll('.nav-ref');
+    navBlocks = document.querySelectorAll('.nav-block');
 
 
 function toggleMenu (event) {
-    if (event.target.matches('.nav-ref')) {
-        event.target.classList.toggle('nav-ref-active');
+    if (event.target.matches('.nav-ref-active')) {
+        event.target.classList.remove('nav-ref-active');
         let info = event.target.nextElementSibling;
-        info.classList.toggle('nav-block-active');
+        info.classList.remove('nav-block-active')
+    } else if (event.target.matches('.nav-ref')) {
+        navRefs.forEach((el) => el.classList.remove('nav-ref-active'));
+        navBlocks.forEach((el) => el.classList.remove('nav-block-active'));
+        event.target.classList.add('nav-ref-active');
+        let info = event.target.nextElementSibling;
+        info.classList.add('nav-block-active');
     }
 };
 
-// function toggleMenu (e) {
-//     if (e.target.classList.contains('nav-ref')) {
-//         console.log ("Yes")
+// function closeMenu (event) {
+//     if (event.target.matches('.nav-ref-active')) {
+//         event.target.classList.remove('nav-ref-active');
+//         let info = event.target.nextElementSibling;
+//         info.classList.remove('nav-block-active');
 //     }
 // }
 
+// navRefs.forEach((el) => el.addEventListener('click', closeMenu));
 navRefs.forEach((el) => el.addEventListener('click', toggleMenu));
