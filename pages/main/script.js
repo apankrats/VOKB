2af102a1aefc21fbfa925184c6d27bc962ffef20
apankrats <aliena0798@mail.ru>
2023-08-10 15:49:02 +0300

    add new lists in main

--- pages/main/script.js
+++ pages/main/script.js
@@ -1,3 +1,5 @@
+// ***navigation***
+
 let navRefs = document.querySelectorAll('.nav-ref');
     navBlocks = document.querySelectorAll('.nav-block');
 
@@ -25,3 +27,24 @@ function toggleMenu (event) {
 
 navRefs.forEach((el) => el.addEventListener('click', toggleMenu));
 // navRefs.forEach((el) => el.addEventListener('click', closeMenu));
+
+// ***centers***
+
+let centerTitle = document.querySelectorAll('.main-centers-title');
+    centerBlocks = document.querySelectorAll('.main-centers-block');
+
+function toggleList (event) {
+    if (event.target.matches('.main-centers-title-active')) {
+        event.target.classList.remove('main-centers-title-active');
+        let info = event.target.nextElementSibling;
+        info.classList.remove('main-centers-block-active')
+    } else if (event.target.matches('.main-centers-title')) {
+        navRefs.forEach((el) => el.classList.remove('main-centers-title-active'));
+        navBlocks.forEach((el) => el.classList.remove('main-centers-block-active'));
+        event.target.classList.add('main-centers-title-active');
+        let info = event.target.nextElementSibling;
+        info.classList.add('main-centers-block-active');
+    }
+};
+
+centerTitle.forEach((el) => el.addEventListener('click', toggleList));
